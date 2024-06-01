17f2d27f34a984120149186571395ce2cfc989b1
ma1iik <108078307+ma1iik@users.noreply.github.com>
2024-06-01 15:02:22 +0200

    SPA, notifs, (#46)
    * minors css changes, blocked user messages dont show in general (done on front), hash routing
    
    * fix
    
    * routing, notifs, css small fixes, blocked user messages dont show in general

--- backend/ft_transcendenceBackend/spa/static/js/webSocket.js
+++ backend/ft_transcendenceBackend/spa/static/js/webSocket.js
@@ -9,9 +9,9 @@ function connectWebSocket() {
         console.log("WebSocket connection established.");
     };
 
-    chatSocket.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-        handleWebSocketMessage(data);
+    chatSocket.onmessage = async function(e) {
+        const data = JSON.parse(e.data);
+        await handleWebSocketMessage(data);
     };
 
     chatSocket.onerror = function(error) {
@@ -23,7 +23,29 @@ function connectWebSocket() {
         setTimeout(connectWebSocket, 1000);
     };
 
-    function handleWebSocketMessage(data) {
+    function msgFromBlocked(source_user_id) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: '/get_block_list/', 
+                method: 'GET',
+                dataType: 'json',
+                success: function(block_list) {
+                    if (block_list.length > 0) {
+                        const isBlocked = block_list.some(block => source_user_id == block.userid);
+                        resolve(isBlocked);
+                    } else {
+                        resolve(false);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                    reject(error);
+                }
+            });
+        });
+    }       
+
+    async  function handleWebSocketMessage(data) {
         switch(data.type) {
             case 'private.message':
                 console.log("websocket: private message");
@@ -31,22 +53,140 @@ function connectWebSocket() {
                 addMessageToChatUI(data.message, data.source_user, data.source_user_id, data.target_user_id);
                 // displayPrivateMessage(data);
                 break;
-            case 'global.message':
-                console.log("websocket: global message");
-                console.log(data.source_user);
-                addMessageToGlobalChatUI(data.message, data.source_user, data.source_user_id);
-                // displayGlobalMessage(data);
-                break;
+                case 'global.message':
+                    console.log("websocket: global message");
+                    console.log(data.source_user);
+                    try {
+                        const isBlocked = await msgFromBlocked(data.source_user_id);
+                        if (isBlocked) {
+                            console.log("Message blocked.");
+                            return;
+                        }
+                        addMessageToGlobalChatUI(data.message, data.source_user, data.source_user_id);
+                    } catch (error) {
+                        console.error("Error checking block list:", error);
+                    }
+                    gameNotification(data);
+                    break;
             case 'notification':
                 displayNotification(data.message);
                 break;
             case 'friendRequest':
                 handleFriendRequest(data);
                 break;
+            case 'game.invite.recieve':
+                gameNotification(data);
+            break;
         }
     }
     window.chatSocket = chatSocket;
 }
+
+function gameNotification(data) {
+    const username = data.source_user;
+    const message = `${username} invited you to a game`;
+    console.log(message);
+
+    $.ajax({
+        url: '/get_notif_translate/',
+        method: 'GET',
+        data: { 'message': message },
+        success: function(response) {
+            if (response && response.translations && response.translations.message) {
+                const notification = document.createElement('div');
+                notification.className = 'game-notification';
+                notification.style.backgroundColor = "rgb(43, 142, 43)";
+                notification.style.color = "white";
+                notification.style.padding = "10px";
+                notification.style.borderRadius = "5px";
+                notification.style.marginBottom = "10px";
+                notification.style.display = "flex";
+                notification.style.alignItems = "center";
+                notification.style.justifyContent = "space-between";
+                notification.style.marginRight = "20px";
+                // notification.style.position = "relative";
+
+                const notificationMessage = document.createElement('span');
+                notificationMessage.textContent = response.translations.message;
+
+                const buttonContainer = document.createElement('div');
+                buttonContainer.style.display = "flex";
+                buttonContainer.style.gap = "8px";
+                buttonContainer.style.marginLeft = "10px";
+
+                const acceptButton = document.createElement('button');
+                acceptButton.className = 'btn btn-link p-0 accept-button';
+                acceptButton.style.border = "none";
+                acceptButton.style.background = "none";
+                acceptButton.innerHTML = '<i class="bi bi-check-circle" style="color: white; font-size: 1.15rem;"></i>';
+                acceptButton.onclick = () => {
+                    acceptGameInvite(data.source_user_id);
+                    notification.remove();
+                    updateNotificationStyles();
+                };
+
+                const declineButton = document.createElement('button');
+                declineButton.className = 'btn btn-link p-0 decline-button';
+                declineButton.style.border = "none";
+                declineButton.style.background = "none";
+                declineButton.innerHTML = '<i class="bi bi-x-circle" style="color: red; font-size: 1.15rem;"></i>';
+                declineButton.onclick = () => {
+                    notification.remove();
+                    updateNotificationStyles();
+                };
+                buttonContainer.appendChild(acceptButton);
+                buttonContainer.appendChild(declineButton);
+                
+                notification.appendChild(notificationMessage);
+                notification.appendChild(buttonContainer);
+
+                const notifications = document.querySelectorAll('.game-notification');
+                if (notifications.length >= 4) {
+                    notifications[0].remove();
+                }
+                document.body.appendChild(notification);
+                updateNotificationStyles();
+                notification.style.display = 'flex';
+                window.scrollTo(0, 0);
+                setTimeout(function() {
+                    notification.remove();
+                    updateNotificationStyles();
+                }, 8000);
+            } else {
+                console.error("Error: Invalid response from the server.");
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error fetching translation:", error);
+        }
+    });
+}
+
+function acceptGameInvite(friendId) {
+    if (window.chatSocket && window.chatSocket.readyState === WebSocket.OPEN) {
+        const inviteMessage = {
+            type: 'game.invite.accept',
+            inviter: friendId,
+            invited: userId
+        };
+        window.chatSocket.send(JSON.stringify(inviteMessage));
+    }
+}
+
+function updateNotificationStyles() {
+    const notifications = document.querySelectorAll('.game-notification');
+    let topPosition = 5;
+    let opacity = 1;
+    const opacityStep = 0.1;
+
+    notifications.forEach((notification, index) => {
+        notification.style.top = `${topPosition}px`;
+        notification.style.opacity = opacity.toFixed(2);
+        topPosition += notification.offsetHeight + 5;
+        opacity -= opacityStep;
+    });
+}
+
 
 function addMessageToGlobalChatUI(message, sender, sender_id) {
     const messageElement = document.createElement('div');
